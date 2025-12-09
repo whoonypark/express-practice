5339ad9c91bd805a4d7836c5ebaad205b8bf0528
JihoonPark <jihoonpark22@ajou.ac.kr>
2025-12-09 16:49:24 +0900

    docker-build fix final

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const firebase = require('./firebase');
 const cors = require('cors');
 
 var app = express()
-const port = 3000
+const port = 8080
 
 app.use(express.json())
 app.use(express.urlencoded({extended : true}));
--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 var firebase = require("firebase-admin");
 
-var serviceAccount = require("./firebaseKey.json");
+var serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount)
